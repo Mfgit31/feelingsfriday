9606d005e6abbb10920d3f69f9acc36df4bbd186
Kimber Herron <kh582013@gmail.com>
2022-11-15 15:05:41 -0500

    mess ups then undid all of it

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,17 +2,30 @@ import React from 'react';
 import '../App.css';
 import StudentPage from './StudentPage';
 import StudentHeader from './StudentHeader';
-
+//import NavBar from "./NavBar";
+//import { Switch, Route, useState } from "react-router-dom";
 
 
 function App() {
+
+  //const [page, setPage] = useState("/")
   return (
     <div className="App">
       <header className="App-header">
       <h1><StudentHeader/></h1>
       <h2>Feelings Friday</h2>
+      {/* <NavBar onChangePage={setPage} />
+      <Switch>
+         
+          <Route exact path="/">
+              <StudentPage />
+          </Route>
+          <Route path="*">
+              <h1>404 not found</h1>
+          </Route>
+      </Switch> */}
       </header>
-    
+
       <StudentPage />
       
     </div>
---
+++ src/Components/NavBar.js
@@ -0,0 +1,19 @@
+// import React from "react";
+// import { Link } from "react-router-dom"
+
+// function NavBar() {
+    
+   
+    
+//     return (
+//         <nav>
+//             <Link to="/">StudentPage</Link>
+
+
+//             {/* <Link to="/about">About</Link>
+//             <Link to="/projects">Projects</a> */}
+//         </nav>
+//     );
+// }
+
+// export default NavBar;
